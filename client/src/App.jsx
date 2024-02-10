63833796266af9bdcd21eaaaa17a6b95a37caea4
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-02-10 12:00:21 +0530

    navbar is done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,28 @@
-import "./App.css";
-
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import Home from "./scenes/homePage/Home";
+import Login from "./scenes/loginPage/Login";
+import ProfilePage from "./scenes/profilePage/ProfilePage";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useSelector } from "react-redux";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 function App() {
-  return <></>;
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
-
 export default App;
