d9076fd592d73389005fbad8893278a01832dd7b
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-02-10 16:15:23 +0530

    home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,8 +10,25 @@ import { themeSettings } from "./theme";
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
   return (
     <div className="app">
+      {/* <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route
+              path="/home"
+              element={isAuth ? <Home /> : <Navigate to="/" />}
+            />
+            <Route path="/" element={<Login />} />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter> */}
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
